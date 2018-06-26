f7eb33affc0272e6902545bb4a619128df4677e1
HRyad <40612103+HRyad@users.noreply.github.com>
2018-06-27 00:35:18 +0100

    V1
    Online version works

---
+++ currency.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+
+function test(){
+
+/*let url = 'https://free.currencyconverterapi.com/api/v5/currencies';
+
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+  console.log('Checkout this JSON! ', out);
+
+})
+.catch(err => { throw err });*/
+
+let sel = document.getElementById("box1");
+let text1= sel.options[sel.selectedIndex].text;
+
+sel = document.getElementById("box2");
+let text2= sel.options[sel.selectedIndex].text;
+
+let amount=document.getElementById("amount").value;
+let val=0; 
+
+ url = 'https://free.currencyconverterapi.com/api/v5/convert?q='+text1+'_'+text2+'&compact=y';
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+for (var key in out) { // the json key CUR_CUR is dynamique
+    val=out[key].val;
+}
+  document.getElementById("result").value = amount*val; 
+
+})
+.catch(err => { throw err });
+
+
+}
