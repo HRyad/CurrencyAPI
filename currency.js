837cc912071cdbd830938cbbf61eac712f018396
HRyad <40612103+HRyad@users.noreply.github.com>
2018-06-27 02:31:32 +0100

    Update currency.js

--- currency.js
+++ currency.js
@@ -29,6 +29,13 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
