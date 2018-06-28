f0896f10b5d64daff400f88b2d1034701d2eb17f
HRyad <40612103+HRyad@users.noreply.github.com>
2018-06-28 13:59:55 +0100

    Update currency.js

--- currency.js
+++ currency.js
@@ -17,31 +17,6 @@ if ('serviceWorker' in navigator) {
 }
 
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-         '/',
-        'CurrencyAPI/currency.js',
-        'CurrencyAPI/index.html',
-        'CurrencyAPI/sw.js'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
-        return caches.open('v1').then(function(cache) {
-          cache.put(event.request, response.clone());
-          return response;
-        });  
-      });
-    })
-  );
-});
 
 
 function test(){
